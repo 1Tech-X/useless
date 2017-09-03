122e886d813779da2e22ca7432d98338d2833f82
1Tech-X <30934772+1Tech-X@users.noreply.github.com>
2017-09-03 19:12:00 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,10 +10,9 @@ echo
 echo " ------------Architecture-----------"
 echo
 echo
-echo "Select Your Phone Architecture"
+echo "SEARCH Your Phone Architecture"
 echo "----------------------------------------------------"
-echo "1)arm64/armv8 (64 bit)"
-echo "2)arm/armhf/armv7 (32 bit)"
-echo "3)64 bit minimal version (try this if option 1 is giving errors)"
-echo "4)find your architecture"
+echo "find your architecture"
 read aarch
+case $aarch in
+1)
